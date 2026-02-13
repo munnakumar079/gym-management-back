771fdf35ef63ab7ae6666230fd198cfb69795b12
Munna Kumar <munnakumar68768@gmail.com>
2026-02-13 07:32:20 +0530

    changes

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Admin\SuperAdminController;
 
 
 Route::post('/register', [AuthController::class, 'register']);
@@ -12,6 +13,15 @@ Route::post('/login', [AuthController::class, 'login']);
 Route::post('/register', [AuthController::class, 'register']);
 
 Route::middleware('auth:api')->group(function () {
-    Route::get('/profile', [AuthController::class, 'profile']);
-    Route::post('/logout', [AuthController::class, 'logout']);
+Route::get('/profile', [AuthController::class, 'profile']);
+Route::post('/logout', [AuthController::class, 'logout']);
+
+
+ Route::get('/owners', [SuperAdminController::class, 'ownerList']);
+ Route::get('/owners/{id}', [SuperAdminController::class, 'viewOwner']);
+ Route::put('/owners/{id}/subscription', [SuperAdminController::class, 'updateSubscription']);
+ Route::delete('/owners/{id}', [SuperAdminController::class, 'deleteOwner']);
+
+
+
 });
